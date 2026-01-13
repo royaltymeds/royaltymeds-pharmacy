335d60472102091f85e6c5a2822dfaad3f93daa6
princewebclient <princewebclient@gmail.com>
2026-01-12 23:29:42 -0500

    feat: add Rx symbol favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "sonner";
 export const metadata: Metadata = {
   title: "RoyaltyMeds - Prescription Platform",
   description: "Order prescriptions online with ease",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
