50f01c17bcd697e57c9ee10aabbbc5a6a71388cb
princewebclient <princewebclient@gmail.com>
2026-01-21 10:00:15 -0500

    Configure Supabase domain for Next.js Image component

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   typescript: {
     tsconfigPath: './tsconfig.json',
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
+  },
   webpack: (config, { isServer }) => {
     // Exclude REFERENCE_APP directory from webpack builds
     config.watchOptions = {
