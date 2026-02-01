aa5d2e51bedd0cf13bb30c450a4bb968eeb3b664
princewebclient <princewebclient@gmail.com>
2026-02-01 03:01:00 -0500

    removed migration file

--- supabase/migrations/20260129000004_add_unindexed_foreign_key_indexes.sql
+++
@@ -1,9 +0,0 @@
--- Add indexes for unindexed foreign keys
--- Improves query performance for foreign key lookups
--- Date: January 29, 2026
-
--- Add index for orders.transaction_id foreign key
-CREATE INDEX IF NOT EXISTS idx_orders_transaction_id ON orders(transaction_id);
-
--- Add index for refill_requests.approved_by foreign key
-CREATE INDEX IF NOT EXISTS idx_refill_requests_approved_by ON refill_requests(approved_by);
